514cff1a9a29e55282bbbd13bd18b730a85c3cec
Allan Anderson <allana@shaw.ca>
2017-05-15 09:40:07 -0700

    End of Project Setup

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+
+#endregion
+namespace ChinookSystem.DAL
+{
+    //This is a internal class for security reasons
+    //Access is restricted(限制) to within this class library project
+    //Inherits DbContext for Entity Framework, which requires
+    //   System.Data.Entity
+    internal class ChinookContext : DbContext
+    {
+        //Pass the connection string name to the
+        //  DbContext using :base()
+        //  i.e. Connection string name is "ChinookDB"
+        public ChinookContext() :base("ChinookDB")
+        {
+
+        }
+    }
+}
